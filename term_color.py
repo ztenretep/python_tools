de7518cd11b55e9aa996a4cecbda5ee546ece05c
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-04-16 17:34:49 +0200

    Update term_color.py

--- term_color.py
+++ term_color.py
@@ -1,17 +1,29 @@
+  GNU nano 4.8                                                                 term_color.py                                                                             
 #!/usr/bin/python3
+"""Terminal color demo."""
 
-fg_col = [30, 31, 32, 33, 34, 35, 36, 37]
-bg_col = [40, 41, 42, 43, 44, 45, 46, 47]
+# Import the standard Python module.
+import sys
 
+# Set the array with foreground and background colors.
+FG_COL = [30, 31, 32, 33, 34, 35, 36, 37]
+BG_COL = [40, 41, 42, 43, 44, 45, 46, 47]
+
+# Set ANSI constants.
 STD = "\033[49m"
 CSI = "\033["
 
-for i in fg_col:
-    for j in bg_col:
-        if i != j-10:
-            fcol_esc = "{0}{1}{2}".format(CSI, str(i), "m")
-            bcol_esc = "{0}{1}{2}".format(CSI, str(j), "m")
-            col_esc = "{0}{1}".format(fcol_esc, bcol_esc)
-            fcol_str = r"\033[" + str(i) + "m"
-            bcol_str = r"\033[" + str(j) + "m"
-            print("{0}{1}{2}{3}".format(col_esc, fcol_str, bcol_str, STD))
+# Loop over the color combinations.
+for fg in FG_COL:
+    for bg in BG_COL:
+        # Print escape code to screen if fg and bg different.
+        if fg != bg-10:
+            fgcol_str = "{0}{1}{2}".format(r"\033[", str(fg), "m")
+            bgcol_str = "{0}{1}{2}".format(r"\033[", str(bg), "m")
+            fgcol_esc_seq = "{0}{1}{2}".format(CSI, str(fg), "m")
+            bgcol_esc_seq = "{0}{1}{2}".format(CSI, str(bg), "m")
+            col_esc_seq = "{0}{1}".format(fgcol_esc_seq, bgcol_esc_seq)
+            fmt_str = "{0}{1}{2}{3}".format(col_esc_seq, fgcol_str, bgcol_str, STD)
+            sys.stdout.write(fmt_str)
+    # Print line break.
+    sys.stdout.write("\n")
