257e0db037a42fd720ac1dbcc5b95cea13b85445
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-04-16 14:01:52 +0200

    Create term_color.py

---
+++ term_color.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+fg_col = [30, 31, 32, 33, 34, 35, 36, 37]
+bg_col = [40, 41, 42, 43, 44, 45, 46, 47]
+
+STD = "\033[49m"
+CSI = "\033["
+
+for i in fg_col:
+    for j in bg_col:
+        if i != j-10:
+            fcol_esc = "{0}{1}{2}".format(CSI, str(i), "m")
+            bcol_esc = "{0}{1}{2}".format(CSI, str(j), "m")
+            col_esc = "{0}{1}".format(fcol_esc, bcol_esc)
+            fcol_str = r"\033[" + str(i)
+            bcol_str = r"\033[" + str(j)
+            print("{0}{1}{2}{3}".format(col_esc, fcol_str, bcol_str, STD))
