9964fa2e68a4cb9f52b0a735e895da2f9f9683dc
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-04-18 08:29:08 +0200

    Update draw_box.py

--- draw_box.py
+++ draw_box.py
@@ -1,49 +1,46 @@
-#!/usr/bin/env python
-"""Create empty frame."""
-# pylint: disable=too-few-public-methods
-# pylint: disable=invalid-name
+#!/usr/bin/env python3
+"""Draw box."""
+# pylint: disable=too-many-instance-attributes
 
-# Set the constants.
+# Set width and height.
 WIDTH = 80
-HEIGHT = 3
+HEIGHT = 20
 
-# =======================
-# Class TableBorderChar()
-# =======================
+# ===============
+# Class DrawBox()
+# ===============
 class DrawBox:
     """Class __init__ function."""
-    # pylint: disable=too-many-instance-attributes
-    def __init__(self, borderchar, width, height):
-        self.borderchar = borderchar
-        self.top_left = borderchar[0]
-        self.top_right = borderchar[1]
-        self.down_left = borderchar[2]
-        self.down_right = borderchar[3]
-        self.horizontal = borderchar[4]
-        self.vertical = borderchar[5]
-        self.width = width
-        self.height = height
+    def __init__(self, border_chars, box_width, box_height):
+        self.chars = border_chars
+        self.width = box_width
+        self.height = box_height
+        self.span = self.width-2
+        self.hspace = ' ' * self.span
+        self.bline = self.chars[4] * self.span
+        self.hline = self.chars[5] + self.hspace + self.chars[5]
 
     def draw_top(self):
-        """Method draw_top()."""
-        span = self.width-2
-        line = self.horizontal * span
-        print(self.top_left + line + self.top_right)
-        return 0
-
-    def draw_mid(self):
-        """Method draw_mid()."""
-        span = self.width-2
-        body = self.vertical + (' ' * span) + self.vertical
-        for _ in range(self.height - 1):
-            print(body)
+        """Class method draw_top()."""
+        print(self.chars[0] + self.bline + self.chars[1])
         return 0
 
     def draw_down(self):
-        """Method draw_down()."""
-        span = self.width-2
-        line = self.horizontal * span
-        print(self.down_left + line + self.down_right)
+        """Class method draw_down()."""
+        print(self.chars[2] + self.bline + self.chars[3])
+        return 0
+
+    def draw_mid(self):
+        """Class method draw_mid()."""
+        for _ in range(self.height - 2):
+            print(self.hline)
+        return 0
+
+    def draw_box(self):
+        """Class method draw_box()."""
+        self.draw_top()
+        self.draw_mid()
+        self.draw_down()
         return 0
 
 # ===================
@@ -56,12 +53,12 @@ def draw_box(box):
     box.draw_down()
 
 # Set the border char lists.
-# TL, TR, DL, DR, H, V
+# TOPLEFT, TOPRIGHT, DOWNLEFT, DOWNRIGHT, HORIZONTAL, VERTICAL
 BC1 = [u'\u250c', u'\u2510', u'\u2514', u'\u2518', u'\u2500', u'\u2502']
 BC2 = [u'\u2554', u'\u2557', u'\u255A', u'\u255D', u'\u2550', u'\u2551']
 
 # Create an instance of DrawBox.
-BORDER = DrawBox(BC1, WIDTH, HEIGHT)
+BOX = DrawBox(BC1, WIDTH, HEIGHT)
 
 # Draw box.
-draw_box(BORDER)
+BOX.draw_box()
