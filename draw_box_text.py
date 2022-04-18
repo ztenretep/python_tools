6a4899394c233b2b5b3e51ef2ba537b71eb38f9d
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-04-18 15:27:00 +0200

    Update draw_box_text.py

--- draw_box_text.py
+++ draw_box_text.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-"""Create a frame arround text."""
+"""Create a frame arround text.
+
+Unicode in the range of U+2500 to U+257F is used for the border chars.
+
+"""
 # pylint: disable=too-many-instance-attributes
 # pylint: disable=too-few-public-methods
+# pylint: disable=consider-using-enumerate
 
 # Set the constants.
 WIDTH = 80
-HEIGHT = 10
+HEIGHT = 8
 
 # Set the text list.
 TEXT = ["This is a text,", "printed for demonstration purposes,",
@@ -48,24 +53,20 @@ class DrawBox:
         """Private method draw_line()."""
         for _ in range(count):
             print(self.chars[5] + self.hspace + self.chars[5])
-        return 0
 
     def __draw_line_text(self, line_count):
         """Private method draw_line()."""
         for count in range(line_count):
             hspc0, _, hspc2 = self.__horizontal_spacing(self.text[count], self.hspan)
             print(self.chars[5] + (' ' * hspc0) + self.text[count] + (' ' * hspc2) + self.chars[5])
-        return 0
 
     def __draw_top(self):
         """Private method draw_top()."""
         print(self.chars[0] + self.bline + self.chars[1])
-        return 0
 
     def __draw_down(self):
         """Private method draw_down()."""
         print(self.chars[2] + self.bline + self.chars[3])
-        return 0
 
     def __draw_mid(self):
         """Private method draw_mid()."""
@@ -73,24 +74,38 @@ class DrawBox:
         self.__draw_line_space(vspc0)
         self.__draw_line_text(vspc1)
         self.__draw_line_space(vspc2)
-        return 0
 
     def draw_box(self):
         """Method draw_box()."""
         self.__draw_top()
         self.__draw_mid()
         self.__draw_down()
-        return 0
 
 # Set the border char lists.
-# BC = [TOPLEFT, TOPRIGHT, DOWNLEFT, DOWNLEFT, HORIZONTAL, VERTICAL]
-BC0 = [u'\u250F', u'\u2513', u'\u2517', u'\u251B', u'\u2501', u'\u2503']
-BC1 = [u'\u250c', u'\u2510', u'\u2514', u'\u2518', u'\u2500', u'\u2502']
-BC2 = [u'\u2554', u'\u2557', u'\u255A', u'\u255D', u'\u2550', u'\u2551']
+# [TOPLEFT, TOPRIGHT, DOWNLEFT, DOWNLEFT, HORIZONTAL, VERTICAL]
+BC = [[u'\u256D', u'\u256E', u'\u2570', u'\u256F', u'\u2500', u'\u2502'],
+      [u'\u250c', u'\u2510', u'\u2514', u'\u2518', u'\u2500', u'\u2502'],
+      [u'\u2554', u'\u2557', u'\u255A', u'\u255D', u'\u2550', u'\u2551'],
+      [u'\u2552', u'\u2555', u'\u2558', u'\u255B', u'\u2550', u'\u2502'],
+      [u'\u2553', u'\u2556', u'\u2559', u'\u255C', u'\u2500', u'\u2551'],
+      [u'\u250F', u'\u2513', u'\u2517', u'\u251B', u'\u2501', u'\u2503'],
+      [u'\u250D', u'\u2511', u'\u2515', u'\u2519', u'\u2501', u'\u2502'],
+      [u'\u250E', u'\u2512', u'\u2516', u'\u251A', u'\u2500', u'\u2503'],
+      [u'\u250F', u'\u2513', u'\u2517', u'\u251B', u'\u2505', u'\u2507'],
+      [u'\u250F', u'\u2513', u'\u2517', u'\u251B', u'\u254D', u'\u254F'],
+      [u'\u250F', u'\u2513', u'\u2517', u'\u251B', u'\u2509', u'\u250B'],
+      [u'\u250C', u'\u2510', u'\u2514', u'\u2518', u'\u2504', u'\u2506'],
+      [u'\u250C', u'\u2510', u'\u2514', u'\u2518', u'\u2508', u'\u250A'],
+      [u'\u250C', u'\u2510', u'\u2514', u'\u2518', u'\u254C', u'\u254E'],
+      ['/', '\\', '\\', '/', '-', '|'],
+      ['+', '+', '+', '+', '-', '|'],
+      ['*', '*', '*', '*', '*', '*'],
+      ['#', '#', '#', '#', '#', '#']]
 
-# Create an instance of DrawBox.
-#BORDER = DrawBox(BC1, TEXT, WIDTH, HEIGHT)
-BORDER = DrawBox(BC0, TEXT, WIDTH, HEIGHT)
-
-# Draw box.
-BORDER.draw_box()
+# Loop over the border strings.
+for num in range(0, len(BC)):
+    print("{0}{1}".format("Frame style: ", num + 1))
+    # Create an instance of DrawBox.
+    BORDER = DrawBox(BC[num], TEXT, WIDTH, HEIGHT)
+    # Draw box.
+    BORDER.draw_box()
