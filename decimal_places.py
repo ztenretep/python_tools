ffcd6cc9e902cefb67eac91c616aa79d86eb7d52
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-04-22 07:34:30 +0200

    Update decimal_places.py

--- decimal_places.py
+++ decimal_places.py
@@ -3,13 +3,20 @@
 
 The script determines the number of decimal places after the decimal point.
 
-The decimal number is divided into two parts. The decimal point acts as a
-separator.Then the number of digits of the decimal part is determined.
-
+First, the decimal number is converted to a string. The string representation
+of the decimal number is divided into two parts. The decimal point acts as a
+separator. Then the number of digits of the decimal part is determined.
 """
 
 # Set the decimal number.
 DECNUM = 3.141592653
 
-# Get the number of decimal places after the decimal point.
-numbers = len(str(DECNUM.split('.')[1])
+def decimal_places(decnum):
+    # Get the number of decimal places after the decimal point.
+    number = len(str(decnum).split('.')[1])
+    # Return the number of decimal places.
+    return number
+
+def main():
+    decimal_places()
+    return None
