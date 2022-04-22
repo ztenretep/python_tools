ee62c20f020767b2696cb5b80dc0eb9b502f1d45
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-04-22 12:42:54 +0200

    Create integer_factors.py

---
+++ integer_factors.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Determine the divisors of an integer number.
+"""
+
+# Declare a list.
+divlst = []
+
+# Get user input.
+intnum = int(input("Provide an integer number: "))
+
+# Initialise counter.
+i=1
+
+while i <= intnum:
+    if intnum % i == 0:
+        factor = intnum // i        
+        divlst.append(factor)
+        i=i+1
+    else:
+        i=i+1
+
+# Get list in reverse order.
+divlst.reverse()
+
+# Print result to screen.
+print(divlst)
