a84e9914f0ed9a0f740ba4b816eee8fdfe0700b1
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-04-22 13:28:47 +0200

    Create harshad_numbers.py

---
+++ harshad_numbers.py
@@ -0,0 +1,15 @@
+#!/use/bin/python3
+"""Calculated the Harshad numbers."""
+
+# Function checksum
+def checksum(n):
+    cs = 0
+    while n > 0:
+        cs = int(cs + (n % 10))
+        n = int(n / 10)
+    return cs
+
+# Print result to screen.
+for i in range (1, 101):
+    if int(i * 10.0 / checksum(i)) % 10 == 0:
+        print(i)
