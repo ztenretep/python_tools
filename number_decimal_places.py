3c139c59ceb38a195359aadd9878f81d9c93d6a4
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-04-24 08:33:37 +0200

    Update number_decimal_places.py

--- number_decimal_places.py
+++ number_decimal_places.py
@@ -5,7 +5,11 @@ Remarks:
 
 Python floating point numbers are internally represented as binary
 rather than decimal numbers. Dealing with decimal places needs strings
-or converting to decimal. 
+or converting float to decimal. A string can converted to decimal, but
+this will tack on zeros to get the needed accuracy, or it will be
+necessary the rounding setting to truncate it. 
+
+Best way seems to be to work with strings to count decimal places.
 
 @params fnum           (str) : String representation of decimal number
 @return decimal_places (int) : Number of decimal places
@@ -13,6 +17,9 @@ or converting to decimal.
 
 def decimal_places_m0(fnum):
     """Method 0.
+   
+    If fnum is of type float the determination of the decimal places
+    will fail. fnum must be of type Str here.
     """
     from decimal import Decimal
     dnum = Decimal(fnum)
