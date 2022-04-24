d4799f330a52b528f23b0f412500ced6e4214506
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-04-24 11:03:30 +0200

    Update number_decimal_places.py

--- number_decimal_places.py
+++ number_decimal_places.py
@@ -25,30 +25,58 @@ def decimal_places_m0(fnum):
     """
     from decimal import Decimal
     dnum = Decimal(fnum)
-    ndp = dnum.as_tuple().exponent
-    if ndp <= 0:
-        return abs(ndp)
+    dexponent = dnum.as_tuple().exponent
+    if dexponent <= 0:
+        decimal_places = abs(dexponent)
     else:
         return 0
+    return decimal_places
 
-def decimal_places_m1(fnum):   
+def decimal_places_m1(fnum):
     """Method 1."""
-    if fnum.find('.') != -1:
-        return len(fnum.split(".")[1])
-    else:
-        return 0
+    from decimal import Decimal
+    dnum = Decimal(fnum)
+    dexponent = dnum.as_tuple().exponent
+    decimal_places = abs(dexponent) if dexponent <= 0 else 0
+    return decimal_places
 
 def decimal_places_m2(fnum):   
     """Method 2."""
-    if '.' in fnum:
-        return len(fnum.split(".")[1])
+    if fnum.find('.') != -1:
+        decimal_places = len(fnum.split(".")[1])
     else:
         return 0
+    return decimal_places
 
-def decimal_places_m3(fnum):
+def decimal_places_m3(fnum):   
     """Method 3."""
-    ndp = fnum[::-1].find('.')
-    if ndp > 0:
-        return ndp
+    if '.' in fnum:
+        decimal_places = len(fnum.split(".")[1])
     else:
         return 0
+    return decimal_places
+
+def decimal_places_m4(fnum):   
+    """Method 4."""
+    decimal_places = len(fnum.split(".")[1]) if '.' in fnum else 0
+    return decimal_places
+
+def decimal_places_m5(fnum):
+    """Method 5."""
+    decimal_places = fnum[::-1].find('.')
+    if decimal_places <= 0:
+        return O
+    return decimal_places
+
+def decimal_places_m6(fnum):
+    """Method 6."""
+    decimal_places = fnum[::-1].find('.') if fnum[::-1].find('.') > 0 else 0
+    return decimal_places
+
+def decimal_places_m7(fnum):
+    """Method 7."""
+    from decimal import Decimal
+    dnum = Decimal(fnum)
+    decimal_places = len(str(abs(dnum - int(dnum)))[2:])
+    return decimal_places
+
