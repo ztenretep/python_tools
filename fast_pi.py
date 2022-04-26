06bc38002516571676a77de7809b24f6fb29fbb1
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-04-26 10:27:05 +0200

    Create fast_pi.py

---
+++ fast_pi.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+# Import Standard Python modules.
+import os
+import math
+
+# Clear screen.
+os.system('clear')
+
+def sqrt(num, prec):
+    """Return the square root of num as a fixed point number prec.
+    """
+    # Set the initial value.
+    n_prec = num * prec
+    # Make an initial guess.
+    float_point_prec = 10 ** 64
+    # Calculate the floating point number.
+    n_float = float((n_prec * float_point_prec) // prec) / float_point_prec
+    # Calculate the initial square root.
+    sr = (int(float_point_prec * math.sqrt(n_float)) * prec) // float_point_prec
+    # Run an infinite loop.
+    while 1:
+        sr_old = sr
+        sr = (sr + n_prec // sr) // 2
+        # Abort criterion.
+        if sr == sr_old:
+            break
+    # Return square root.
+    return sr
+
+def pi_chudnovsky(digits=1000000):
+    """Calculate pi using Chudnovsky
+    """
+    # Set the constants.
+    d0 = 13591409
+    d1 = 545140134
+    n1 = 426880
+    n2 = 10005
+    c1 = 640320
+    c0 = 24
+    c = c1*c1*c1 // c0
+    # Set the initial values.
+    a_k = digits
+    a_sum = digits
+    b_sum = 0
+    k = 1
+    while 1:
+        a_k *= -(6*k-5)*(2*k-1)*(6*k-1)
+        a_k //= k*k*k*c
+        a_sum += a_k
+        b_sum += k * a_k
+        k += 1
+        if a_k == 0:
+            break
+    total = d0 * a_sum + d1 * b_sum
+    pi = (n1 * sqrt(n2, digits) * digits) // total
+    # Return Pi.
+    return pi
+
+# Set number of digits.
+exp = 10 ** 128
+
+# Print Pi.
+print(pi_chudnovsky(exp))
